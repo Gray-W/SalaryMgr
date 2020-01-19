ddbd175aa5c1743eb24f8cac5b0a34fca9a3fcdd
Gray <gray_wang8320@163.com>
2020-01-19 14:36:08 +0800

    'addsytle

--- tmpUse.py
+++ tmpUse.py
@@ -20,6 +20,13 @@ def _caculateDaySalary(countList, ratio):
 def main():
     dt = datetime.datetime.now()
     payroll = xlsxwriter.Workbook('%s-%s-%s工资表.xlsx' % (dt.year, dt.month, dt.day))
+    bold = payroll.add_format({
+        'bold': True,
+        'font_size': 15,
+    })
+    baseFormat = payroll.add_format({
+        'font_size': 14,
+    })
 
     with open('月工资输入.txt', 'r', encoding='utf8') as f:
         dataList = _readline(f)
@@ -35,6 +42,7 @@ def main():
             totalSalary = 0
             day = 1
             workSheet = payroll.add_worksheet(name)
+            workSheet.set_column('B:H', 15)
             headings = ['id', '计件', '计件', '计件', '计件', '计件', '日工', '当日工资']
 
             while True:  # 日工资
@@ -42,10 +50,9 @@ def main():
                 if dataList[0].isalpha() or not dataList[0]:
                     print('------------------------------', name, ": ", "总工资：", totalSalary, '----------------------------------------------------')
                     print()
-                    workSheet.write_row('A1', headings)
-                    workSheet.write_row('A%s' % (day+1), ['', '', '', '', '', '', '总工资:', totalSalary])
+                    workSheet.write_row('A1', headings, bold)
+                    workSheet.write_row('A%s' % (day+1), ['', '', '', '', '', '', '总工资:', totalSalary], bold)
                     break
-                daySalary = 0
                 daySalary, haveDayWork = _caculateDaySalary(dataList, ratio)
                 totalSalary += daySalary
                 print(day, ": ", daySalary)
@@ -57,13 +64,15 @@ def main():
                         dataList.insert(-2, '')
                     else:
                         dataList.insert(-1, '')
-                workSheet.write_row('A%s' % (day+1), dataList)
+                workSheet.write_row('A%s' % (day+1), dataList, baseFormat)
                 day += 1
+
         payroll.close()
 
 
 if __name__ == '__main__':
     main()
+    # input('输任意键关闭窗口，直接关也行！')
 
 
 
